991d50c1c66f0eabaa2a7b95627338d4095207df
AlondraPliego <alondrapliego131104@gmail.com>
2026-07-03 22:04:24 -0600

    Moficiacion de ejercicio 14

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -1,7 +1,5 @@
 function Calcular() {
     let cantidades = document.getElementById("cantidades").value;
-    let arreglo = cantidades.split(",");
-    let cantidadesF = arreglo.map(Number);
     let mayor = document.getElementById("mayor");
     let menor = document.getElementById("menor");
     let promedio = document.getElementById("promedio");
@@ -15,16 +13,18 @@ function Calcular() {
         document.getElementById("cantidades").focus();
         return;
     }
+    let arreglo = cantidades.split(",");
+    let cantidadesF = arreglo.map(Number);
     if (cantidadesF.some(isNaN)) {
-    mensajeError.textContent = "Todos los valores deben ser números válidos.";
-    document.getElementById("cantidades").focus();
-    return;
-}
-let myr=Math.max(...cantidadesF);
-        document.getElementById("mayor").value = myr;
-let mnr=Math.min(...cantidadesF);
-        document.getElementById("menor").value = mnr;
-let suma=cantidadesF.reduce((acc, valor) => acc+valor, 0);
-let pmr=suma / cantidadesF.length;
-        document.getElementById("promedio").value = pmr.toFixed(3);
+        mensajeError.textContent = "Todos los valores deben ser números válidos.";
+        document.getElementById("cantidades").focus();
+        return;
+    }
+    let myr = Math.max(...cantidadesF);
+    mayor.value = myr;
+    let mnr = Math.min(...cantidadesF);
+    menor.value = mnr;
+    let suma = cantidadesF.reduce((acc, valor) => acc + valor, 0);
+    let pmr = suma / cantidadesF.length;
+    promedio.value = pmr.toFixed(3);
 }
